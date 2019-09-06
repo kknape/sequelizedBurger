52f7a8b9a0c4f39a6213495402148c2badc54e64
k2k <kadonna@wonderkul.com>
2019-09-06 13:10:37 -0500

    Updated the skeleton files and model.

---
+++ models/burger.js
@@ -0,0 +1,20 @@
+module.exports = function(sequelize, DataTypes) {
+  var Burger = sequelize.define("Burger", {
+    // Giving the Burger model a name of type STRING
+    burger_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [1]
+      }
+    },
+    devoured: {
+      type: DataTypes.BOOLEAN,
+      allowNull: false,
+      validate: {
+        len: [1]
+      }
+    }
+  });
+  return Burger;
+};
