f435a99713317d8583faf9bd860bc2494d7d37ad
k2k <kadonna@wonderkul.com>
2019-09-06 15:19:44 -0500

    Creating HTML page

---
+++ routes/burger-api-routes.js
@@ -0,0 +1,30 @@
+var db = require("../models");
+
+module.exports = function(app) {
+  app.get("/api/burgers", function(req, res) {
+    // Here we add an "include" property to our options in our findAll query
+    // We set the value to an array of the models we want to include in a left outer join
+    // In this case, just db.Post
+    db.Burger.findAll({
+      include: [db.Burger]
+    }).then(function(dbBurger) {
+      res.json(dbBurger);
+    });
+  });
+
+  app.post("/api/burgers", function(req, res) {
+    db.Burger.create(req.body).then(function(dbBurger) {
+      res.json(dbBurger);
+    });
+  });
+
+  app.delete("/api/burgers/:id", function(req, res) {
+    db.Burger.destroy({
+      where: {
+        id: req.params.id
+      }
+    }).then(function(dbBurger) {
+      res.json(dbBurger);
+    });
+  });
+};
